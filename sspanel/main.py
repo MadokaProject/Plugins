6cc8ffe802fbaece8a074d2f0480c399de1b8ba0
Colsrch <colsrch@foxmail.com>
2022-09-30 21:24:53 +0800

    部分插件更新写法

--- sspanel/main.py
+++ sspanel/main.py
@@ -1,15 +1,17 @@
-from typing import Union
-
 import requests
-from arclet.alconna import Alconna, Args, Subcommand, Arpamar
-from graia.ariadne.app import Ariadne
-from graia.ariadne.model import Friend, Member, Group
-from graia.scheduler import GraiaScheduler, timers
 from loguru import logger
 from peewee import fn
 
 from app.core.app import AppCore
-from app.core.commander import CommandDelegateManager
+from app.util.alconna import Args, Subcommand, Arpamar, Commander
+from app.util.graia import (
+    Ariadne,
+    Friend,
+    FriendMessage,
+    GraiaScheduler,
+    timers,
+    message,
+)
 from app.util.phrases import *
 from .database.database import PluginSspanelAccount as DBSspanel
 
@@ -18,138 +20,152 @@ requests.packages.urllib3.disable_warnings()
 core: AppCore = AppCore()
 app: Ariadne = core.get_app()
 sche: GraiaScheduler = core.get_scheduler()
-manager: CommandDelegateManager = CommandDelegateManager()
 
 
-@manager.register(
-    entry='sspanel',
-    brief_help='机场签到',
-    alc=Alconna(
-        headers=manager.headers,
-        command='sspnael',
-        options=[
-            Subcommand('qd', help_text='立即进行一次签到', args=Args['host', 'url']['email', 'email']['password', str]),
-            Subcommand('add', help_text='添加/修改签到账号', args=Args['host', 'url']['email', 'email']['password', str]),
-            Subcommand('remove', help_text='删除签到账号', args=Args['host', 'url']['email', 'email']),
-            Subcommand('list', help_text='列出你添加的签到账号')
-        ],
-        help_text='机场签到: 为保证账号安全, 该服务仅私发有效'
-    ))
-async def process(target: Union[Friend, Member], sender: Union[Friend, Group], command: Arpamar, alc: Alconna):
-    subcommand = command.subcommands
-    if not subcommand:
-        return await print_help(alc.get_help())
-    try:
-        if not isinstance(sender, Friend):
-            return MessageChain([Plain('请私聊使用该命令!')])
-        if qd := subcommand.get('qd'):
-            account = {0: {
-                'web': qd['host'],
-                'user': qd['email'],
-                'pwd': qd['password']
-            }}
-            msg = await checkin(account)
-            return MessageChain([
-                Plain('机场签到完成\r\n'),
-                Plain(msg)
-            ])
-        elif add := subcommand.get('add'):
-            DBSspanel.replace(qid=target.id, web=add['host'], user=add['email'], pwd=add['password']).execute()
-            return MessageChain('添加/修改成功！')
-        elif remove := subcommand.get('remove'):
-            DBSspanel.delete().where(
-                DBSspanel.qid == target.id,
-                DBSspanel.web == remove['host'],
-                DBSspanel.user == remove['email']
-            ).execute()
-            return MessageChain('删除成功！')
-        elif command.find('list'):
-            return MessageChain('\n'.join(
-                f'{index}: {res.web}\t{res.user}' for index, res in
-                enumerate(DBSspanel.select().where(DBSspanel.qid == target.id))
-            ))
-        return args_error()
-    except Exception as e:
-        logger.exception(e)
-        return unknown_error()
+command = Commander(
+    "sspanel",
+    "机场签到",
+    Subcommand(
+        "qd",
+        help_text="立即进行一次签到",
+        args=Args["host", "url"]["email", "email"]["password", str],
+    ),
+    Subcommand(
+        "add",
+        help_text="添加/修改签到账号",
+        args=Args["host", "url"]["email", "email"]["password", str],
+    ),
+    Subcommand(
+        "remove", help_text="删除签到账号", args=Args["host", "url"]["email", "email"]
+    ),
+    Subcommand("list", help_text="列出你添加的签到账号"),
+    help_text="机场签到: 为保证账号安全, 该服务仅私发有效",
+)
 
 
-@sche.schedule(timers.crontabify('0 8 * * * 0'))
+@command.parse("qd", events=[FriendMessage])
+async def qd_sspanel(sender: Friend, cmd: Arpamar):
+    account = {
+        0: {
+            "web": cmd.query("host"),
+            "user": cmd.query("email"),
+            "pwd": cmd.query("password"),
+        }
+    }
+    msg = await checkin(account)
+    message([Plain("机场签到完成\r\n"), Plain(msg)]).target(sender).send()
+
+
+@command.parse("add", events=[FriendMessage])
+async def add_sspanel(sender: Friend, cmd: Arpamar):
+    DBSspanel.replace(
+        qid=sender.id,
+        web=cmd.query("host"),
+        user=cmd.query("email"),
+        pwd=cmd.query("password"),
+    ).execute()
+    message("添加/修改成功！").target(sender).send()
+
+
+@command.parse("remove", events=[FriendMessage])
+async def remove_sspanel(sender: Friend, cmd: Arpamar):
+    DBSspanel.delete().where(
+        DBSspanel.qid == sender.id,
+        DBSspanel.web == cmd.query("host"),
+        DBSspanel.user == cmd.query("email"),
+    ).execute()
+    message("删除成功！").target(sender).send()
+
+
+@command.parse("list", events=[FriendMessage])
+async def list_sspanel(sender: Friend):
+    message(
+        "\n".join(
+            f"{index}: {res.web}\t{res.user}"
+            for index, res in enumerate(
+                DBSspanel.select().where(DBSspanel.qid == sender.id)
+            )
+        )
+    ).target(sender).send()
+
+
+@sche.schedule(timers.crontabify("0 8 * * * 0"))
 async def tasker():
     for res in DBSspanel.select(
-            DBSspanel.qid,
-            fn.GROUP_CONCAT(DBSspanel.web, '||').alias('group_web'),
-            fn.GROUP_CONCAT(DBSspanel.user, '||').alias('group_user'),
-            fn.GROUP_CONCAT(DBSspanel.pwd, '||').alias('group_pwd')
+        DBSspanel.qid,
+        fn.GROUP_CONCAT(DBSspanel.web, "||").alias("group_web"),
+        fn.GROUP_CONCAT(DBSspanel.user, "||").alias("group_user"),
+        fn.GROUP_CONCAT(DBSspanel.pwd, "||").alias("group_pwd"),
     ).group_by(DBSspanel.qid):
         accounts = {
-            index: {
-                'web': web,
-                'user': user,
-                'pwd': pwd
-            } for index, (web, user, pwd) in enumerate(zip(
-                res.group_web.replace('||,', '||').strip('||').split('||'),
-                res.group_user.replace('||,', '||').strip('||').split('||'),
-                res.group_pwd.replace('||,', '||').strip('||').split('||')
-            ))
+            index: {"web": web, "user": user, "pwd": pwd}
+            for index, (web, user, pwd) in enumerate(
+                zip(
+                    res.group_web.replace("||,", "||").strip("||").split("||"),
+                    res.group_user.replace("||,", "||").strip("||").split("||"),
+                    res.group_pwd.replace("||,", "||").strip("||").split("||"),
+                )
+            )
         }
         msg = await checkin(accounts)
         await message_send(msg, int(res.qid))
 
 
 async def checkin(account):
-    msgall = ''
+    msgall = ""
     for i in account.keys():
 
-        email = account[i]['user'].split('@')
-        email = email[0] + '%40' + email[1]
-        password = account[i]['pwd']
+        email = account[i]["user"].split("@")
+        email = f"{email[0]}%40{email[1]}"
+        password = account[i]["pwd"]
 
         session = requests.session()
 
         try:
             # 以下except都是用来捕获当requests请求出现异常时，
             # 通过捕获然后等待网络情况的变化，以此来保护程序的不间断运行
-            session.get(account[i]['web'], verify=False)
+            session.get(account[i]["web"], verify=False)
 
         except requests.exceptions.ConnectionError:
-            msg = account[i]['web'] + '\n\n' + '网络不通'
-            msgall = msgall + account[i]['web'] + '\n\n' + msg + '\n\n'
+            msg = account[i]["web"] + "\n\n" + "网络不通"
+            msgall = msgall + account[i]["web"] + "\n\n" + msg + "\n\n"
             logger.warning(msg)
             continue
         except requests.exceptions.ChunkedEncodingError:
-            msg = account[i]['web'] + '\n\n' + '分块编码错误'
-            msgall = msgall + account[i]['web'] + '\n\n' + msg + '\n\n'
+            msg = account[i]["web"] + "\n\n" + "分块编码错误"
+            msgall = msgall + account[i]["web"] + "\n\n" + msg + "\n\n"
             logger.warning(msg)
             continue
         except Exception as e:
-            msg = account[i]['web'] + '\n\n' + '未知错误'
-            msgall = msgall + account[i]['web'] + '\n\n' + msg + '\n\n'
+            msg = account[i]["web"] + "\n\n" + "未知错误"
+            msgall = msgall + account[i]["web"] + "\n\n" + msg + "\n\n"
             logger.warning(msg)
             logger.exception(e)
             continue
 
-        login_url = account[i]['web'] + '/auth/login'
+        login_url = account[i]["web"] + "/auth/login"
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
         }
 
-        post_data = 'email=' + email + '&passwd=' + password + '&code='
+        post_data = f"email={email}&passwd={password}&code="
         post_data = post_data.encode()
         session.post(login_url, post_data, headers=headers, verify=False)
 
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
-            'Referer': account[i]['web'] + '/user'
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+            "Referer": account[i]["web"] + "/user",
         }
 
-        response = session.post(account[i]['web'] + '/user/checkin', headers=headers, verify=False)
-        msg = (response.json()).get('msg')
+        response = session.post(
+            account[i]["web"] + "/user/checkin", headers=headers, verify=False
+        )
+        msg = (response.json()).get("msg")
 
-        msgall = msgall + account[i]['web'] + '\n\n' + msg + '\n\n'
+        msgall = msgall + account[i]["web"] + "\n\n" + msg + "\n\n"
 
-        info_url = account[i]['web'] + '/user'
+        info_url = account[i]["web"] + "/user"
         session.get(info_url, verify=False)
 
     return msgall
@@ -157,7 +173,4 @@ async def checkin(account):
 
 async def message_send(msg, qid):
     """签到消息推送"""
-    await app.send_friend_message(qid, MessageChain([
-        Plain('机场签到完成\r\n'),
-        Plain(msg)
-    ]))
+    message([Plain("机场签到完成\r\n"), Plain(msg)]).target(qid).send()
--- word_cloud/main.py
+++ word_cloud/main.py
@@ -2,22 +2,27 @@ import re
 import datetime
 from io import BytesIO
 from pathlib import Path
-from typing import Union
+from typing import Optional
 
 import jieba.analyse
 import numpy
 from PIL import Image as IMG
-from arclet.alconna import Alconna, Subcommand, Arpamar
-from graia.ariadne.message.element import At
-from graia.ariadne.model import Friend, Member, Group
 from loguru import logger
 from matplotlib import pyplot
 from wordcloud import WordCloud, ImageColorGenerator
 
-from app.core.commander import CommandDelegateManager
 from app.plugin.basic.__01_sys.database.database import Msg as DBMsg
+from app.util.alconna import Subcommand, Arpamar, Commander
+from app.util.graia import (
+    At,
+    Member,
+    MessageChain,
+    Group,
+    GroupMessage,
+    message,
+)
+from app.util.control import Permission
 from app.util.phrases import *
-from app.util.send_message import safeSendGroupMessage
 from app.util.tools import to_thread
 
 BASEPATH = Path(__file__).parent
@@ -28,67 +33,84 @@ STOPWORDS = BASEPATH.joinpath("stopwords")
 RUNNING = 0
 RUNNING_LIST = []
 
-manager: CommandDelegateManager = CommandDelegateManager()
+
+command = Commander(
+    "词云",
+    "词云",
+    Subcommand("个人", help_text="查看个人词云"),
+    Subcommand("本群", help_text="查看本群词云"),
+)
 
 
-@manager.register(
-    entry='词云',
-    brief_help='词云',
-    alc=Alconna(
-        headers=manager.headers,
-        command='词云',
-        options=[
-            Subcommand('个人', help_text='查看个人词云'),
-            Subcommand('本群', help_text='查看本群词云')
-        ],
-        help_text='词云'
-    ))
-async def process(target: Union[Friend, Member], sender: Union[Friend, Group], command: Arpamar, alc: Alconna):
+@command.parse("个人", events=[GroupMessage])
+async def my_word_cloud(target: Member, sender: Group):
     global RUNNING, RUNNING_LIST
-    if not command.subcommands:
-        return await print_help(alc.get_help())
-    try:
-        if not isinstance(sender, Group):
-            return MessageChain([Plain('请在群组使用该命令！')])
-        if RUNNING < 5:
-            RUNNING += 1
-            RUNNING_LIST.append(target.id)
-            if command.find('个人'):
-                talk_list = DBMsg.select(DBMsg.content).where(
-                    DBMsg.uid == sender.id,
-                    DBMsg.qid == target.id,
-                    DBMsg.datetime >= datetime.datetime.now() - datetime.timedelta(days=7)
-                )
-            else:
-                talk_list = DBMsg.select(DBMsg.content).where(
-                    DBMsg.uid == sender.id,
-                    DBMsg.datetime >= datetime.datetime.now() - datetime.timedelta(days=7)
-                )
-            talk_list = [
-                re.sub(r'[0-9]+', '', talk).strip('@')
-                for talk in (MessageChain.from_persistent_string(msg.content).display for msg in talk_list)
-                if talk not in ['[图片]'] or talk[0] not in '.,;!?。，；！？/\\'
-            ]
-            if len(talk_list) < 10:
-                await safeSendGroupMessage(sender, MessageChain([Plain("当前样本量较少，无法制作")]))
-                RUNNING -= 1
-                RUNNING_LIST.remove(target.id)
-                return
-            await safeSendGroupMessage(sender, MessageChain(
-                [At(target.id), Plain(f" 正在制作词云，一周内共 {len(talk_list)} 条记录")]
-            ))
-            words = await get_frequencies(talk_list)
-            image = await to_thread(make_wordcloud, words)
-            await safeSendGroupMessage(sender, MessageChain([
-                At(target.id), Plain(f" 已成功制作{'个人' if command.find('个人') else '本群'}词云"), Image(data_bytes=image)]
-            ))
+    if RUNNING < 5:
+        RUNNING += 1
+        RUNNING_LIST.append(target.id)
+        talk_list = DBMsg.select(DBMsg.content).where(
+            DBMsg.uid == sender.id,
+            DBMsg.qid == target.id,
+            DBMsg.datetime >= datetime.datetime.now() - datetime.timedelta(days=7),
+        )
+        if image := await make_word_cloud(target, sender, talk_list):
+            message(
+                [
+                    At(target.id),
+                    Plain(f" 已成功制作{'个人' if command.find('个人') else '本群'}词云"),
+                    Image(data_bytes=image),
+                ]
+            ).target(sender).send()
             RUNNING -= 1
             RUNNING_LIST.remove(target.id)
-        else:
-            await safeSendGroupMessage(sender, MessageChain([Plain("词云生成进程正忙，请稍后")]))
-    except Exception as e:
-        logger.exception(e)
-        return unknown_error()
+    else:
+        message("词云生成进程正忙，请稍后").target(sender).send()
+
+
+@command.parse("本群", events=[GroupMessage], permission=Permission.GROUP_ADMIN)
+async def group_word_cloud(target: Member, sender: Group):
+    global RUNNING, RUNNING_LIST
+    if RUNNING < 5:
+        RUNNING += 1
+        RUNNING_LIST.append(target.id)
+        talk_list = DBMsg.select(DBMsg.content).where(
+            DBMsg.uid == sender.id,
+            DBMsg.datetime >= datetime.datetime.now() - datetime.timedelta(days=7),
+        )
+        if image := await make_word_cloud(target, sender, talk_list):
+            message(
+                [
+                    At(target.id),
+                    Plain(f" 已成功制作{'个人' if command.find('个人') else '本群'}词云"),
+                    Image(data_bytes=image),
+                ]
+            ).target(sender).send()
+            RUNNING -= 1
+            RUNNING_LIST.remove(target.id)
+    else:
+        message("词云生成进程正忙，请稍后").target(sender).send()
+
+
+async def make_word_cloud(
+    target: Member, sender: Group, talk_list: list
+) -> Optional[bytes]:
+    talk_list = [
+        re.sub(r"[0-9]+", "", talk).strip("@")
+        for talk in (
+            MessageChain.from_persistent_string(msg.content).display
+            for msg in talk_list
+        )
+        if talk not in ["[图片]"] or talk[0] not in ".,;!?。，；！？/\\"
+    ]
+    if len(talk_list) < 10:
+        message("当前样本量较少，无法制作").target(sender).send()
+        RUNNING -= 1
+        RUNNING_LIST.remove(target.id)
+        return
+    message(f" 正在制作词云，一周内共 {len(talk_list)} 条记录").at(target).target(sender).send()
+    words = await get_frequencies(talk_list)
+    image: bytes = await to_thread(make_wordcloud, words)
+    return image
 
 
 async def get_frequencies(msg_list):
